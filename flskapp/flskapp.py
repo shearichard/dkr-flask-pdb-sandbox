cadf4bfa6a0e88f889262a72feec19871ad617bb
Richard Shea <rshea@thecubagroup.com>
2021-05-18 21:32:30 +1200

    Add an API endpoint which takes you into a PDB session

--- flskapp/flskapp.py
+++ flskapp/flskapp.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def a_function_to_step_into(n, m):
+    output = n + m
+    return output
+
+    
+@app.route("/hellopdb")
+def hello_pdb():
+    somedic = {"reason": 'only here for pdb to look at'}
+    somedic['return_value'] = a_function_to_step_into(5,6)
+    import pdb;pdb.set_trace()
+    return "<h1>flaskapp</h1><h2>Hello, PDB!</h2><p><a href='/'>Home page</a>.</p>"
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return '''<h1>flaskapp</h1><h2>Hello, World!</h2><p>If you want to try debugging <a href='/hellopdb'>click here</a>.</p>'''
